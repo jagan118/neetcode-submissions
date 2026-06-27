7517898d79659ea3cbe8cff5b2f7e4b1a6060057
Avula jagan <jaganachari2006@gmail.com>
2026-06-27 09:57:38 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+
+        for(let i= 0;i<nums.length;i++){
+            if(nums.includes(target - nums[i])){
+                return [i,nums.indexOf(target - nums[i])];
+            }
+        }
+
+    }
+}
